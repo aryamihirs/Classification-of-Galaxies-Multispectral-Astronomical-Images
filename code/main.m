eaeb100cacd891bea270c614660194abb6fa774c
aryamihirsingh <arya.m.singh.10@gmail.com>
2016-11-28 04:22:32 +0530

    main.m added

---
+++ code/main.m
@@ -0,0 +1,18 @@
+str_s1 = '../dataset2/s1';
+im_s1 = imread('../dataset2/s1/s1_1.jpg');
+str_s2 = '../dataset2/s2';
+im_s2 = imread('../dataset2/s2/s2_1.jpg');
+str_e1 = '../dataset2/e1';
+im_e1 = imread('../dataset2/e1/e1_1.jpg');
+str_e2 = '../dataset2/e2';
+im_e2 = imread('../dataset2/e2/e2_1.jpg');
+
+MaskedIm_s1 = segment(str_s1,im_s1);
+MaskedIm_s2 = segment(str_s2,im_s2);
+MaskedIm_e1 = segment(str_e1,im_e1);
+MaskedIm_e2 = segment(str_e2,im_e2);
+
+classifier(MaskedIm_s1);
+classifier(MaskedIm_s2);
+classifier(MaskedIm_e1);
+classifier(MaskedIm_e2);
--- code/segment.m
+++ code/segment.m
@@ -1,89 +1,90 @@
-clc;
-clear all;
-imset = imageSet('../dataset2/e1');
-seg_d = zeros(124,124,imset.Count);
-seg_e = zeros(124,124,imset.Count);
-area_e = zeros(1,imset.Count);
-area_d = zeros(1,imset.Count);
-gravity_c = [[62 63] [62 65] [66 62] [63 63]];
-for i=1:imset.Count
-    x = read(imset,i);
-%     x = imread('./s1/s1_1.jpg');
-    x = imresize(x,[124 124]);   
-    x = rgb2gray(x);
-    se = strel('disk',5);
-    op = imopen(x,se);
-    opcl = imclose(op,se);
-    cl = imclose(x,se);
-    clop = imopen(cl,se);
-    occo = (opcl + clop)/2;
-    occo = imgaussfilt(occo);
-    % imshow(occo);
-    h = imhist(occo);
-    th = 0;
-    for j = 1:256
-        th = th + (j-1)*h(j);
-    end
-    th = th / (124*124);
-    % th = uint8(th);
-    level = th / 255;
-    bw = im2bw(occo,level);
-%     bw = im2bw(occo);
-    bw = imopen(bw,se);
+function MaskedImg = segment(str,im)
+    imset = imageSet(str);
+    seg_d = zeros(124,124,imset.Count);
+    seg_e = zeros(124,124,imset.Count);
+    area_e = zeros(1,imset.Count);
+    area_d = zeros(1,imset.Count);
+    gravity_c = [[62 63] [62 65] [66 62] [63 63]];
+    for i=1:imset.Count
+        x = read(imset,i);
+    %     x = imread('./s1/s1_1.jpg');
+        x = imresize(x,[124 124]);   
+        x = rgb2gray(x);
+        se = strel('disk',5);
+        op = imopen(x,se);
+        opcl = imclose(op,se);
+        cl = imclose(x,se);
+        clop = imopen(cl,se);
+        occo = (opcl + clop)/2;
+        occo = imgaussfilt(occo);
+    % imshow(occo); 
+        h = imhist(occo);
+        th = 0;
+        for j = 1:256
+            th = th + (j-1)*h(j);
+        end
+        th = th / (124*124);
+        % th = uint8(th);
+        level = th / 255;
+        bw = im2bw(occo,level);
+    %     bw = im2bw(occo);
+        bw = imopen(bw,se);
     
-    cc = bwconncomp(1-bw);
-    surface_cc = cellfun(@numel,cc.PixelIdxList);
+        cc = bwconncomp(1-bw);
+        surface_cc = cellfun(@numel,cc.PixelIdxList);
     
-    ch = bwconvhull(1-bw,'objects',8);
-    ch_cc = bwconncomp(ch);
-    surface_ch = cellfun(@numel,ch_cc.PixelIdxList);
-    ratio_c = double(max(surface_cc)) / double(max(surface_ch));
-    e_bw = imerode(1-bw,se);
-    e_bw_cc = bwconncomp(e_bw);
-    surface_e_cc = cellfun(@numel,e_bw_cc.PixelIdxList);
-    ratio_e = double(max(surface_e_cc)) / double(max(surface_cc));
-    
-    while ratio_e > ratio_c;
-        e_bw = imerode(e_bw,se);
+        ch = bwconvhull(1-bw,'objects',8);
+        ch_cc = bwconncomp(ch);
+        surface_ch = cellfun(@numel,ch_cc.PixelIdxList);
+        ratio_c = double(max(surface_cc)) / double(max(surface_ch));
+        e_bw = imerode(1-bw,se);
         e_bw_cc = bwconncomp(e_bw);
         surface_e_cc = cellfun(@numel,e_bw_cc.PixelIdxList);
         ratio_e = double(max(surface_e_cc)) / double(max(surface_cc));
-    end
+    
+        while ratio_e > ratio_c;
+            e_bw = imerode(e_bw,se);
+            e_bw_cc = bwconncomp(e_bw);
+            surface_e_cc = cellfun(@numel,e_bw_cc.PixelIdxList);
+            ratio_e = double(max(surface_e_cc)) / double(max(surface_cc));
+        end
     
     
-    d_bw = imdilate(1-bw,se);
-    d_bw_cc = bwconncomp(d_bw);
-    surface_d_cc = cellfun(@numel,d_bw_cc.PixelIdxList);
-    ratio_d = double(max(surface_d_cc)) / double(max(surface_cc));
-    
-    while ratio_d < 2 - ratio_c;
-        d_bw = imdilate(d_bw,se);
+        d_bw = imdilate(1-bw,se);
         d_bw_cc = bwconncomp(d_bw);
-        surface_d_cc = cellfun(@numel,d_bw_cc.PixelIdxList);
+        surface_d_cc = cellfun(@numel,d_bw_cc.PixelIdxList);    
         ratio_d = double(max(surface_d_cc)) / double(max(surface_cc));
+    
+        while ratio_d < 2 - ratio_c;
+            d_bw = imdilate(d_bw,se);
+            d_bw_cc = bwconncomp(d_bw);
+            surface_d_cc = cellfun(@numel,d_bw_cc.PixelIdxList);
+            ratio_d = double(max(surface_d_cc)) / double(max(surface_cc));
+        end
+        
+        MinArNumPixels = cellfun(@numel,d_bw_cc.PixelIdxList);  
+        [biggest,idx] = min(MinArNumPixels);
+        d_bw(d_bw_cc.PixelIdxList{idx}) = 0;
+    
+        seg_e(:,:,i) = e_bw;
+        seg_d(:,:,i) = d_bw;
+        area_e(i) = max(surface_e_cc);
+        area_d(i) = max(surface_d_cc);
+    
+    %     figure;
+    %     subplot(2,2,1);imshow(bw);
+    %     subplot(2,2,2);imshow(ch);
+    %     subplot(2,2,3);imshow(e_bw);  
+    %     subplot(2,2,4);imshow(d_bw);
     end
-    
-    MinArNumPixels = cellfun(@numel,d_bw_cc.PixelIdxList);
-    [biggest,idx] = min(MinArNumPixels);
-    d_bw(d_bw_cc.PixelIdxList{idx}) = 0;
-    
-    seg_e(:,:,i) = e_bw;
-    seg_d(:,:,i) = d_bw;
-    area_e(i) = max(surface_e_cc);
-    area_d(i) = max(surface_d_cc);
-    
-%     figure;
-%     subplot(2,2,1);imshow(bw);
-%     subplot(2,2,2);imshow(ch);
-%     subplot(2,2,3);imshow(e_bw);
-%     subplot(2,2,4);imshow(d_bw);
-end
-[val_maxd idx_maxd] = max(area_d);
-final_d = seg_d(:,:,idx_maxd);
-B_d = bwboundaries(final_d);
-imshow(final_d);
-hold on
-for k = 1:length(B_d)
-   boundary = B_d{k};
-   plot(boundary(:,2), boundary(:,1), 'b', 'LineWidth', 1)
+    [val_maxd idx_maxd] = max(area_d);
+    final_d = seg_d(:,:,idx_maxd);
+    B_d = bwboundaries(final_d);
+    imshow(im);
+    MaskedImg = final_d .* im;
+    hold on
+    for k = 1:length(B_d)
+       boundary = B_d{k};
+       plot(boundary(:,2), boundary(:,1), 'b', 'LineWidth', 1)
+    end
 end
