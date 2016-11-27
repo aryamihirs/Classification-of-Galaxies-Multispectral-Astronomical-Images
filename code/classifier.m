2c894cf0e1bd401b020d600fe8c6a43b53eb020a
kryptonish <darthnishious@gmail.com>
2016-11-28 04:32:18 +0530

    re test

--- code/classifier.m
+++ code/classifier.m
@@ -1,11 +1,11 @@
-function op = classifier(mask, image)
-I1 = imread('gal11.jpg');
-I1 = medfilt2(I1);
+function op = classifier(maskedImage)
+I1 = medfilt2(maskedImage);
 SE = strel('disk',5,5);
 i1 = I1 - imopen(I1,SE);
 I1 = imbinarize(i1);
-BW2 = bwareaopen(I1,)
-imshow(i1);
+BW2 = bwareaopen(I1,20);
+
+imshow(BW2);
 
 
 
